8f42dd545d789e9931e361e782abe88a82e08e7a
Zanets <darg20127@gmail.com>
2018-11-15 02:28:39 +0800

    2

--- main.go
+++ main.go
@@ -7,9 +7,21 @@ import (
 	"path/filepath"
 )
 
+type NodeType int
+
+
+const (
+	File NodeType = 0
+	Directory NodeType = 1
+)
+
 type Node struct {
 	Name string 
+	Path string
+	Type NodeType
+	Level int
 }
+
 
 func isGit(path string) bool {
 	return false	
@@ -22,26 +34,34 @@ func drawBranch(name string, level int) {
 	fmt.Println("|-" + name)
 }
 
-func walk(path string, level int) {
+func walk(path string, level int) []Node {
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	
+	var nodes []Node
 	for _, file := range files {
-		drawBranch(file.Name(), level)
+		// drawBranch(file.Name(), level)
+		name := file.Name()
+		if name == ".git" {
+			continue
+		}
 		if file.IsDir() {
-			walk(path + "/" + file.Name(), level+1)
+			nodes = append(nodes, walk(path + "/" + file.Name(), level+1)...)
+		} else {
+			nodes = append(nodes, Node{file.Name(), path, File, level})
 		}
 	}
+	return nodes
 }
 
 func main() {
 	target, err := filepath.Abs("./")
 	fmt.Println(target)
 	if err == nil {
-		walk(target, 1)
+		nodes := walk(target, 1)
+		fmt.Println(nodes)
 	} else {
 		log.Fatal(err)
 	}
